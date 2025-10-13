151bff5963052b3a91e9c1d4339af6c9e28cf441
Pat Kearns <pat.kearns@dbtlabs.com>
2025-10-13 05:12:04 +0000

    remove unused macro

--- macros/log_sources.sql
+++
@@ -1,8 +0,0 @@
-{% macro log_sources(results) %}
---$----
-  {%- for result in results if result.node.resource_type in ['models', 'sources'] -%}
-    --$$----
-        {{ log(result | tojson(indent=4), info=True) }}
-    {% endfor %}
-    
-{% endmacro %}
